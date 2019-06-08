80abe5ca3e0c93996c112b7b09d0c23d6df3428b
langshiquan <576506402@qq.com>
2019-06-08 22:01:36 +0800

    sort v2

--- src/main/java/com/baidu/sort/QuickSort.java
+++ src/main/java/com/baidu/sort/QuickSort.java
@@ -45,4 +45,40 @@ public class QuickSort {
         return leftIndex;
     }
 
+
+    // 单边循环
+    public static void sortV2(Integer[] elements) {
+        quickSortV2(elements, 0, elements.length - 1);
+    }
+
+    private static void quickSortV2(Integer[] elements, int startIndex, int endIndex) {
+        System.out.println(Arrays.toString(elements));
+        if (startIndex >= endIndex) {
+            return;
+        }
+        int pivotIndex = partitionV2(elements, startIndex, endIndex);
+
+        // 中间是ok的，不用放入递归比较
+        quickSort(elements, startIndex, pivotIndex - 1);
+        quickSort(elements, pivotIndex + 1, endIndex);
+    }
+
+    private static int partitionV2(Integer[] elements, int startIndex, int endIndex) {
+        int pivot = elements[startIndex];
+        int mark = startIndex;
+        // 基准位置不比较交互
+        for (int i = startIndex + 1; i < endIndex; i++) {
+            if (elements[i] < pivot) {
+                mark++;
+                int temp = elements[i];
+                elements[i] = elements[mark];
+                elements[mark] = temp;
+            }
+        }
+        elements[startIndex] = elements[mark];
+        elements[mark] = pivot;
+        return mark;
+    }
+
+
 }
--- src/test/java/com/baidu/sort/QuickSortTest.java
+++ src/test/java/com/baidu/sort/QuickSortTest.java
@@ -16,4 +16,12 @@ public class QuickSortTest {
         assertEquals("[1, 1, 2, 6, 6, 24, 32, 34, 34, 34, 45, 63, 72, 234, 234, 345, 345, 534]", Arrays.asList(numbers).toString());
     }
 
+    @Test
+    public void quickSortV2() {
+        Integer[] numbers = new Integer[]{34, 534, 63, 34, 32, 34, 234, 234, 1, 345, 6, 72, 24, 2, 1, 6, 45, 345};
+        QuickSort.sortV2(numbers);
+        assertEquals("[1, 1, 2, 6, 6, 24, 32, 34, 34, 34, 45, 63, 72, 234, 234, 345, 345, 534]", Arrays.asList(numbers).toString());
+    }
+
+
 }
